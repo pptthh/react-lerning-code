69b438d190349d1322a2c2a2e7f65ef0a7ccf398
pptthh <pptthh@users.noreply.github.com>
2018-11-23 11:59:10 +0100

    Task 1

---
+++ src/components/PureComponentExample.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class PureComponentExample extends React.PureComponent {
+  render() {
+    return <h1>This is a React.PureComponent example</h1>;
+  }
+}
+
+export default PureComponentExample;
