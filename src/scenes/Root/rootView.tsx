8564b13abce7e8d6fc04a7cdbeca40bc02ef0398
Péter Tóth <peter_toth1@epam.com>
2019-01-11 16:45:48 +0100

    main page changes

--- src/scenes/Root/rootView.tsx
+++ src/scenes/Root/rootView.tsx
@@ -9,56 +9,28 @@ import store from './rootStore';
 
 const search = (props: SrUrlProps) => <Provider store={store}><SearchResults {...props}/></Provider>;
 const film = (props: DvUrlProps) => <Provider store={store}><DetailedView  {...props}/></Provider>;
-
-const Home = () => <div><h2>Home</h2></div>;
-const NotFound = () => <div><h2>page not found</h2></div>;
+const links = () =>
+<ul>
+    <li><Link to='/'>home</Link></li>
+    <li><Link to='/search'>search</Link></li>
+    <li><Link to='/search/'>search/</Link></li>
+    <li><Link to='/search/pulp'>search/pulp</Link></li>
+    <li><Link to='/film'>film</Link></li>
+    <li><Link to='/film/'>film/</Link></li>
+    <li><Link to='/film/353081'>film/353081</Link></li>
+    <li><Link to='/404'>404 not found</Link></li>
+</ul>;
+const Home = () => <div><h2>Home</h2>{links()}</div>;
+const NotFound = () => <div><h2>The linked page was not found</h2>{links()}</div>;
 
 const Root = () =>
 <Router>
-<>
-    <ul>
-        <li><Link to='/'>home</Link></li>
-        <li><Link to='/search'>search</Link></li>
-        <li><Link to='/search/'>search/</Link></li>
-        <li><Link to='/search/pulp'>search/pulp</Link></li>
-        <li><Link to='/film'>film</Link></li>
-        <li><Link to='/film/'>film/</Link></li>
-        <li><Link to='/film/353081'>film/353081</Link></li>
-        <li><Link to='/404'>404 not found</Link></li>
-    </ul>
-    <hr/>
     <Switch>
         <Route path='/' component={Home}  exact={true}/>
         <Route path={srUrlPath} component={search} />
         <Route path={dvUrlPath} component={film} />
         <Route component={NotFound} />
     </Switch>
-</>
 </Router>;
 
 export default Root;
-
-// const About = () => <div><h2>About</h2></div>;
-
-// const Topics = ({ match }: any) =>
-
-// <div>
-// <h2>Topics</h2>
-// <ul>
-//     <li>
-//     <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-//     </li>
-//     <li>
-//     <Link to={`${match.url}/components`}>Components</Link>
-//     </li>
-//     <li>
-//     <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-//     </li>
-// </ul>
-
-// <Route path={`${match.path}/:topicId`} component={Topic} />
-// <Route exact={true} path={match.path} render={() => <h3>Please select a topic.</h3>}
-// />
-// </div>;
-
-// const Topic = ({ match }: any) => <div><h3>{match.params.topicId}</h3></div>;
