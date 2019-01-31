54e4acf0e6d9d4899a5b62559eee1b08de4e6bee
Péter Tóth <peter_toth1@epam.com>
2019-01-31 11:22:52 +0100

    lint config

--- src/ssr/server.test.ts
+++ src/ssr/server.test.ts
@@ -2,12 +2,12 @@ import { Response } from 'express';
 // tslint:disable-next-line
 const http = require('http');
 const PORT = process.env.port || '8888';
-const SERVER_URL = 'http://localhost:' + PORT;
+const SERVER_URL = `http://localhost:${PORT}`;
 const HTTP_STATUS_OK = 200;
 const HTTP_STATUS_ERROR = 400;
 
 describe('Node Server Test', () => {
-    it('should return 200 on: ' + SERVER_URL, done => {
+    it(`should return less then 400 on: ${SERVER_URL}`, done => {
         http.get(SERVER_URL, (res: Response) => {
             expect(res.statusCode).toBeLessThan(HTTP_STATUS_ERROR);
             // expect(res.statusCode).toEqual(HTTP_STATUS_OK);
