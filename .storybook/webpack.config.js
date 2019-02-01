767ac52c8d1f85e3df94826ae2c0a79a6c9cabf6
Péter Tóth <peter_toth1@epam.com>
2019-02-01 11:45:38 +0100

    story book add config for typescript

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,14 +5,15 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
-
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      // add your custom rules.
-    ],
-  },
+const path = require('path');
+module.exports = (baseConfig, env, config) => {
+    config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
+            {loader: require.resolve('awesome-typescript-loader')},
+            {loader: require.resolve('react-docgen-typescript-loader')}
+        ]
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
 };
